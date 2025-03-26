77274d8d6225060efedfcb8271ccf338a7f2a8c9
hideaki-kawai <hideaki.kawai@thephage.life>
2025-03-27 01:50:24 +0900

    fix: 不要なファイル削除

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
 export default flatRoutes({
-  ignoredRouteFiles: ["**/*.test.*"], // 無視するファイルパターンを設定
+  ignoredRouteFiles: ["**/*.test.*"],
 }) satisfies RouteConfig;
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,9 +2,6 @@ import type { LoaderFunctionArgs } from "react-router";
 import { redirect } from "react-router";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  // ChatページでNested Route使いたかったから / はひとまずリダイレクト
   return redirect("/chats/new");
-}
-
-export default function Index() {
-  return <></>;
 }
