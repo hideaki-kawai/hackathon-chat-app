9340d0a5e8d626d779a12e0461ada424b11d5c50
hideaki-kawai <hideaki.kawai@thephage.life>
2025-06-02 15:37:49 +0900

    fix

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,9 @@
 import { type RouteConfig } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
+/**
+ * @returns ルート
+ */
 export default flatRoutes({
   ignoredRouteFiles: ["**/*.test.*"],
 }) satisfies RouteConfig;
